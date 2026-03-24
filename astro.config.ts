2c470e5f12e12410a2c0ac9f4c5a41c431de0c6a
JannaP <janna.putseva@epitech.eu>
2026-03-24 11:36:36 +0100

    fixed astro.config

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel({ ... }),
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
